5a9c346ccfa96976755486154dd22fcfb98899f7
seonghanim <tjdgksdl1120@gamil.com>
2025-10-29 11:29:54 +0900

    init

---
+++ src/main/java/com/han/bloomi/BloomiApplication.java
@@ -0,0 +1,13 @@
+package com.han.bloomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomiApplication.class, args);
+    }
+
+}
